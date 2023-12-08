6735551eb8196e109261953611f86d058659a360
hameji <hameji@gmail.com>
2023-12-08 14:02:15 -0500

    second commit

--- blog/models.py
+++ blog/models.py
@@ -6,6 +6,7 @@ from django.utils import timezone
 class Post(models.Model):
     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     title = models.TextField()
+    text = models.TextField(default='')
     created_date = models.DateTimeField(default=timezone.now)
     published_date = models.DateTimeField(blank=True, null=True)
 
